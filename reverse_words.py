17cdc0fbfa27e2b5932ab7deea46c1f4922a3b3e
Ravinder Baid <ravinderbaid@gmail.com>
2020-07-16 17:03:16 +0530

    Day 15 reverse words

---
+++ reverse_words.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s=s.strip().split(" ")[::-1]
+        wrd=""
+        for i in s:
+            if i.strip():
+                wrd+=i+" "
+        return wrd[:-1]
+        
