ce64e752f1e8a68980a1c9433f4c5b333b73a97f
Ravinder Baid <ravinderbaid@gmail.com>
2020-07-21 22:50:38 +0530

    Made code more better by removing board from going in backtrack. As board would add an extra space with recursion stack

--- word_search.py
+++ word_search.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def backtrack(i, j, word, l, r, board):
+        def backtrack(i, j, word, l, r):
             if len(word) == 0:
                 return True
             elif i < 0 or j < 0 or i >= l or j >= r or board[i][j] != word[0]:
@@ -11,19 +11,16 @@ class Solution:
             s = board[i][j]
             board[i][j] = "#"
             result = (
-                backtrack(i, j + 1, word[1:], l, r, board)
-                or backtrack(i, j - 1, word[1:], l, r, board)
-                or backtrack(i + 1, j, word[1:], l, r, board)
-                or backtrack(i - 1, j, word[1:], l, r, board)
+                backtrack(i, j + 1, word[1:], l, r)
+                or backtrack(i, j - 1, word[1:], l, r)
+                or backtrack(i + 1, j, word[1:], l, r)
+                or backtrack(i - 1, j, word[1:], l, r)
             )
             board[i][j] = s
             return result
 
         for i in range(len(board)):
-            for j in range(len(board[i])):
-                if board[i][j] == word[0]:
-                    if backtrack(
-                        i, j, word[1:], len(board), len(board[i]), board
-                    ):
-                        return True
+            for j in range(len(board[0])):
+                if backtrack(i, j, word, len(board), len(board[0])):
+                    return True
         return False
