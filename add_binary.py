4da1788520187c22eb4d4669aa7e3b107befac96
Ravinder Baid <ravinderbaid@gmail.com>
2020-07-22 11:49:10 +0530

    Added code to remove bin function from add_binary.

--- add_binary.py
+++ add_binary.py
@@ -1,3 +1,45 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a, 2) + int(b, 2)).replace("0b", "")
+        i = len(a) - 1
+        j = len(b) - 1
+        res = ""
+        carry = "0"
+        while i > -1 and j > -1:
+            sum = int(a[i]) + int(b[j])
+            if sum == 2:
+                if carry == "1":
+                    res += "1"
+                else:
+                    res += "0"
+                    carry = "1"
+            elif sum == 1:
+                if carry == "1":
+                    res += "0"
+                else:
+                    res += "1"
+            else:
+                res += carry
+                carry = "0"
+            i -= 1
+            j -= 1
+        if i < j:
+            i = j
+            a = b
+        while i > -1:
+            if a[i] == "1":
+                if carry == "1":
+                    res += "0"
+                else:
+                    res += "1"
+            elif carry == "1":
+                res += "1"
+                carry = "0"
+            else:
+                res += "0"
+            i -= 1
+
+        if carry != "0":
+            res += "1"
+
+        return res[::-1]
+        # return bin(int(a,2) + int(b,2)).replace("0b","")
