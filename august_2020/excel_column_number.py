a538885f0e10077cc93b5eb99133689d977c6d76
Ravinder Baid <ravinderbaid@gmail.com>
2020-08-10 18:20:58 +0530

    August, Day 10 Problem Type - String, maths, calculations Added question type in comments

--- august_2020/excel_column_number.py
+++ august_2020/excel_column_number.py
@@ -1,4 +1,18 @@
 class Solution:
+    """
+    Given a column title as appear in an Excel sheet, return its corresponding column number.
+    For example:
+
+        A -> 1
+        B -> 2
+        C -> 3
+        ...
+        Z -> 26
+        AA -> 27
+        AB -> 28
+        ...
+    """
+
     def titleToNumber(self, s: str) -> int:
         str_index = 0
         num = 0
